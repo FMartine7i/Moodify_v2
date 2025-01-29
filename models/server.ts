f93219f6c5833ffa141a278191b8d760e6e13121
Federico Martinetti <frmmartinetti98@gmail.com>
2025-01-29 04:32:14 -0300

    cambios en server.ts

--- models/server.ts
+++ models/server.ts
@@ -5,7 +5,9 @@ import cors from 'cors'
 import songsRouter from '../routes/songs.js'
 import albumsRouter from '../routes/albums.js'
 import playlistsRouter from '../routes/playlists.js'
+import usersRouter from '../routes/users.js'
 import connectDB from '../database/conex.js'
+import http from 'http'
 
 class Server {
   private app: Application
@@ -28,13 +30,14 @@ class Server {
     this.app.use('/api/v2/songs', songsRouter)
     this.app.use('/api/v2/albums', albumsRouter)
     this.app.use('/api/v2/playlists', playlistsRouter)
+    this.app.use('/api/v2/users', usersRouter)
   }
   // conexión a base de datos
   public async listen() {
     try {
       await connectDB()
       this.app.listen(this.port, () => {
-        console.log(`Servidor corriendo en puerto ${this.port}`)
+        console.log(`Servidor corriendo en http://:${this.port}`)
       })
     } catch (err) {
         console.error('Error al conectar a la base de datos:', err)
